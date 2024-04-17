02408863aae6065637c88ddb0b3d4322c86ee98f
Felix S <116848884+latsonj@users.noreply.github.com>
2024-04-17 23:14:50 +1200

    Create game board module

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-console.log('Hello, World!');
+let gameBoard = (function() {
+  const row1 = ["A", "B", "C"];
+  const row2 = ["A", "B", "C"];
+  const row3 = ["A", "B", "C"];
+  
+  return {
+    row1,
+    row2,
+    row3,
+  }
+})();
+
+console.log(gameBoard);
